74eb275bbdf5678cb9fac0e70b71abd4a1f1626d
Vovchyk <volodymyr.galas@gmail.com>
2016-11-12 21:21:10 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors','on');
+include './vendor/autoload.php';
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+$mail->addAddress('volodymyr.galas@gmail.com');
+// echo 'Done';
+?>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="/style/style.css" rel="stylesheet" type="text/css" >
+    <script src="/vendor/public/jquery/dist/jquery.min.js"></script>
+</head>
+<body>
+<h1>vvg.com</h1>
+</body>
+</html>
